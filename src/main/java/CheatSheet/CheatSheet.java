e2c533c6a8a7d089733e4455cf1300475fb2391e
E0407750 <e0407750@u.nus.edu>
2020-10-07 13:02:02 +0800

    Added class methods to CheatSheet class

--- src/main/java/CheatSheet/CheatSheet.java
+++ src/main/java/CheatSheet/CheatSheet.java
@@ -1,2 +1,47 @@
-package CheatSheet;public class CheatSheet {
+package CheatSheet;
+
+public class CheatSheet {
+    private String cheatSheetName;
+    private String cheatSheetProgrammingLanguage;
+    private String cheatSheetDetails;
+
+    public CheatSheet(String cheatSheetName, String cheatSheetProgrammingLanguage, String cheatSheetDetails) {
+        this.cheatSheetName = cheatSheetName;
+        this.cheatSheetProgrammingLanguage = cheatSheetProgrammingLanguage;
+        this.cheatSheetDetails = cheatSheetDetails;
+    }
+
+    public String getCheatSheetName() {
+        return cheatSheetName;
+    }
+
+    public String getCheatSheetProgrammingLanguage() {
+        return cheatSheetProgrammingLanguage;
+    }
+
+    public String getCheatSheetDetails() {
+        return cheatSheetDetails;
+    }
+
+    public void setCheatSheetName(String cheatSheetName) {
+        this.cheatSheetName = cheatSheetName;
+    }
+
+    public void setCheatSheetProgrammingLanguage(String cheatSheetProgrammingLanguage) {
+        this.cheatSheetProgrammingLanguage = cheatSheetProgrammingLanguage;
+    }
+
+    public void setCheatSheetDetails(String cheatSheetDetails) {
+        this.cheatSheetDetails = cheatSheetDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "CheatSheet{" +
+                "cheatSheetName='" + cheatSheetName + '\'' +
+                ", cheatSheetProgrammingLanguage='" + cheatSheetProgrammingLanguage + '\'' +
+                ", cheatSheetDetails='" + cheatSheetDetails + '\'' +
+                '}';
+    }
 }
+
