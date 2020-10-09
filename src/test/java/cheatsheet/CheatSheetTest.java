c8535dd7485fd6f1fd8d9459a7dcb028ddf3735d
E0407750 <e0407750@u.nus.edu>
2020-10-09 12:03:06 +0800

    Fixed import  statement for CheatSheet JUnit testing

--- src/test/java/cheatsheet/CheatSheetTest.java
+++ src/test/java/cheatsheet/CheatSheetTest.java
@@ -2,7 +2,7 @@ package cheatsheet;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CheatSheetTest {
 
