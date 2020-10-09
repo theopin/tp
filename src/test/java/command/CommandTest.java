4326feba99d3d8c1fef564ad69f1c7462092f5ea
abnermtj <abnermtj@live.com.sg>
2020-10-09 21:39:34 +0800

    Add command test

---
+++ src/test/java/command/CommandTest.java
@@ -0,0 +1,14 @@
+package command;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CommandTest {
+
+    @Test
+    public void testCommand(){
+        // todo : implement this
+        assertTrue(true);
+    }
+}
