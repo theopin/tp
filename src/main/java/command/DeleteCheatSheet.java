c0a988b799f8f886280e77bf1775af5ac892e5fb
brandontoh <60382521+brandontoh@users.noreply.github.com>
2020-10-12 16:01:52 +0800

    Add execute() for some Commands (#40)
    * Edit Welcome and Exit message
    
    * Edit Parser to return description and command type
    
    * Add CommandExecute
    
    * Remove content in input.txt and EXPECTED.TXT
    
    * Edit EXPECTED.TXT
    
    * Comment out newSession()
    
    * Remove content from EXPECTED.TXT
    
    * Delete extra newline
    
    * Fix EXPEXTED.TXT
    
    * Add execute() for some Commands

--- src/main/java/command/DeleteCheatSheet.java
+++ src/main/java/command/DeleteCheatSheet.java
@@ -1,5 +1,8 @@
 package command;
 
+import cheatsheet.CheatSheet;
+import cheatsheet.CheatSheetList;
+import parser.ArgumentEnum;
 import parser.Parser;
 
 public class DeleteCheatSheet extends Command {
@@ -9,6 +12,13 @@ public class DeleteCheatSheet extends Command {
 
     @Override
     public void execute() {
-
+        String name = "";
+        if (parser.getDescriptionMap().containsKey(ArgumentEnum.NAME)) {
+            name = parser.getDescriptionMap().get(ArgumentEnum.NAME);
+        }  else if (parser.getDescriptionMap().containsKey(ArgumentEnum.INDEX)) {
+            int index = Integer.parseInt(parser.getDescriptionMap().get(ArgumentEnum.INDEX));
+            name = CheatSheetList.getCheatSheet(index).getCheatSheetName();
+        }
+        CheatSheetList.remove(name);
     }
 }
