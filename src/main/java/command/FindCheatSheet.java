0712629ca6cdbbe4915847398af8d7877270980c
brandontoh <60382521+brandontoh@users.noreply.github.com>
2020-10-12 14:02:45 +0800

    Add CommandExecute (#39)
    * Edit Welcome and Exit message
    
    * Edit Parser to return description and command type
    
    * Add CommandExecute
    
    * Remove content in input.txt and EXPECTED.TXT
    
    * Edit EXPECTED.TXT
    
    * Comment out newSession()
    
    * Remove content from EXPECTED.TXT
    
    * Delete extra newline
    
    * Fix EXPEXTED.TXT

--- src/main/java/command/FindCheatSheet.java
+++ src/main/java/command/FindCheatSheet.java
@@ -1,6 +1,12 @@
 package command;
 
+import parser.Parser;
+
 public class FindCheatSheet extends Command {
+    public FindCheatSheet(Parser parser) {
+        super(parser);
+    }
+
     @Override
     public void execute() {
 
--- src/main/java/command/Help.java
+++ src/main/java/command/Help.java
@@ -1,6 +1,12 @@
 package command;
 
+import parser.Parser;
+
 public class Help extends Command {
+    public Help(Parser parser) {
+        super(parser);
+    }
+
     @Override
     public void execute() {
 
