03ba804c185fd0163bf915508431aeb5a2170d6f
brandontoh <60382521+brandontoh@users.noreply.github.com>
2020-10-22 15:08:22 +0800

    Fix duplicated name bug for AddCommand (#77)
    * Fix delete using name bug
    
    * Remove some comments
    
    * Enable assertion in build.gradle
    
    * Add JUnit testing for AddCommand
    
    * Fix duplicated name bug for AddCommand
    
    * Fix indentation
    
    * Comment a repeated part in CheatSheetListTest
    
    * Add sort capability in ListCommand
    
    * Minor changes to sort and ListCommand
    
    * Add sort to FindCommand
    
    * Automatically copy details to clipboard
    
    * Edit input and expected file
    
    * Comment out a test
    
    * Comment out a viewCommand test
    
    * Update build.gradle
    
    Co-authored-by: abnermtj <55742254+abnermtj@users.noreply.github.com>

--- src/main/java/command/FindCommand.java
+++ src/main/java/command/FindCommand.java
@@ -5,9 +5,12 @@ import cheatsheet.CheatSheetList;
 import exception.CommandException;
 import parser.ArgumentFlagEnum;
 import parser.Parser;
+import sort.SortByLanguage;
+import sort.SortByName;
 import ui.Printer;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FindCommand extends Command {
     public FindCommand(Parser parser) {
@@ -52,6 +55,38 @@ public class FindCommand extends Command {
         for (CheatSheet cs : cheatSheetArrayList) {
             Printer.printCheatSheet(cs);
             Printer.printWhiteSpace();
+        }
+        askForSortingConfigAndPrint(cheatSheetArrayList);
+    }
+
+    protected void askForSortingConfigAndPrint(ArrayList<CheatSheet> cheatSheetArrayList) {
+        System.out.println("Sort filter (na: name ascending, la: language ascending, nd: name descending"
+            + ", ld: language descending or <enter> to skip)");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.isEmpty()) {
+            switch (input) {
+            case "na":
+                cheatSheetArrayList.sort(new SortByName());
+                break;
+            case "la":
+                cheatSheetArrayList.sort(new SortByLanguage());
+                break;
+            case "nd":
+                cheatSheetArrayList.sort(new SortByName().reversed());
+                break;
+            case "ld":
+                cheatSheetArrayList.sort(new SortByLanguage().reversed());
+                break;
+            default:
+                cheatSheetArrayList.sort(new SortByName());
+            }
+            System.out.println("Showing all matches: ");
+            for (CheatSheet cs : cheatSheetArrayList) {
+                Printer.printCheatSheet(cs);
+                Printer.printWhiteSpace();
+            }
+            input = scanner.nextLine();
         }
     }
 }
