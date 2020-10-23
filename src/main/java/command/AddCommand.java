0883ee4458670e07b865797da846a9c2c6fafd88
E0407750 <e0407750@u.nus.edu>
2020-10-24 00:21:27 +0800

    Initial implementation of editor class
    still quite buggy

--- src/main/java/command/AddCommand.java
+++ src/main/java/command/AddCommand.java
@@ -2,9 +2,10 @@ package command;
 
 import cheatsheet.CheatSheet;
 import cheatsheet.CheatSheetList;
+import editor.Editor;
+import exception.CommandException;
 import parser.ArgumentFlagEnum;
 import parser.Parser;
-import exception.CommandException;
 import ui.Printer;
 
 public class AddCommand extends Command {
@@ -14,19 +15,28 @@ public class AddCommand extends Command {
 
     @Override
     public void execute() throws CommandException {
+        boolean isExit;
         String name = parser.getDescriptionMap().get(ArgumentFlagEnum.NAME);
         String programmingLanguage = parser.getDescriptionMap().get(ArgumentFlagEnum.PROGRAMMINGLANGUAGE);
-        String description = parser.getDescriptionMap().get(ArgumentFlagEnum.DESCRIPTION);
-
+        //String description = parser.getDescriptionMap().get(ArgumentFlagEnum.DESCRIPTION);
+        Editor contentEditor = new Editor();
+        do {
+            //wait for the program to finish
+            // needs to figure out how to stop the infinite loop
+            System.out.println("Loading....");
+            isExit = contentEditor.isEditDone();
+        } while (!isExit);
+        System.out.println("HELLO IM HERE");
+        String description = contentEditor.getContent();
         if (name == null) {
             throw new CommandException("Please enter a name");
         } else if (checkIfNameAlreadyExist(name)) {
             throw new CommandException("Name already existed, please enter another name");
         }
-
         CheatSheet cheatSheet = new CheatSheet(name, programmingLanguage, description);
         CheatSheetList.add(cheatSheet);
         Printer.printAddNewCheatSheetMessage(cheatSheet);
+
     }
 
     private boolean checkIfNameAlreadyExist(String name) {
---
+++ src/main/java/editor/Editor.java
@@ -0,0 +1,71 @@
+package editor;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Editor extends JFrame implements ActionListener {
+    JTextArea textArea;
+    JFrame editorFrame;
+    static String typedText;
+    static boolean isEditDone;
+
+    public Editor(){
+        isEditDone = false;
+        editorFrame = new JFrame("editor");
+        textArea = new JTextArea();
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu menu1 = new JMenu("Actions");
+
+        JMenuItem menuItem1 = new JMenuItem("Save");
+        JMenuItem menuItem2 = new JMenuItem( "Clear All");
+        JMenuItem menuItem3 = new JMenuItem("Cancel");
+
+        menuItem1.addActionListener(this);
+        menuItem2.addActionListener(this);
+        menuItem3.addActionListener(this);
+
+        menu1.add(menuItem1);
+        menu1.add(menuItem2);
+        menu1.add(menuItem3);
+
+        menuBar.add(menu1);
+
+        editorFrame.setJMenuBar(menuBar);
+        editorFrame.add(textArea);
+        editorFrame.setSize(600,800);
+        editorFrame.show();
+    }
+
+    public void actionPerformed(ActionEvent a){
+        String action = a.getActionCommand();
+        switch (action) {
+        case "Save":
+            toggleEdit();
+            typedText = textArea.getText();
+            //editorFrame.dispose();
+            break;
+        case "Clear All":
+            textArea.setText(" ");
+            break;
+        case "Cancel":
+            textArea.setText(" ");
+            editorFrame.setVisible(false);
+            break;
+        }
+    }
+
+    private void toggleEdit(){
+        isEditDone = true;
+    }
+
+    public boolean isEditDone() {
+        return isEditDone;
+    }
+
+    public String getContent(){
+        return typedText;
+    }
+}
