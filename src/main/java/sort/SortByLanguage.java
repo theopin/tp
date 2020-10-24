4f0f2b4eaf449ac414df7922b421a0ddd090fe43
adhy-p <54772103+adhy-p@users.noreply.github.com>
2020-10-24 12:26:34 +0800

    Add print by favourite (#85)
    * Add clear method for CheatSheetList
    
    * Resolve merge conflict
    
    * Add methods to remove and getCheatSheet by index and by name
    
    * Fix DataFileReaderTest, add text-ui-test
    
    * Add assertions and logger for CheatSheetList
    
    * Improve CheatSheetList methods error messages
    
    * Implement print CSList by fav followed by name/lang

--- src/main/java/sort/SortByLanguage.java
+++ src/main/java/sort/SortByLanguage.java
@@ -6,6 +6,12 @@ import java.util.Comparator;
 
 public class SortByLanguage implements Comparator<CheatSheet> {
     public int compare(CheatSheet a, CheatSheet b) {
+        Boolean cheatSheet1 = a.getIsFavourite();
+        Boolean cheatSheet2 = b.getIsFavourite();
+        int favComparator = cheatSheet2.compareTo(cheatSheet1);
+        if (favComparator != 0) {
+            return favComparator;
+        }
         return a.getCheatSheetProgrammingLanguage().compareTo(b.getCheatSheetProgrammingLanguage());
     }
 }
