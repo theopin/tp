85f3ef03138e7797d2c00af6e16197a89edc71f4
E0407750 <e0407750@u.nus.edu>
2020-10-26 22:08:47 +0800

    Reworked Editor to have cut, copy, and paste

--- src/main/java/command/AddCommand.java
+++ src/main/java/command/AddCommand.java
@@ -4,6 +4,7 @@ import cheatsheet.CheatSheet;
 import cheatsheet.CheatSheetList;
 import editor.Editor;
 import exception.CommandException;
+import exception.EditorException;
 import parser.CommandFlag;
 import ui.Printer;
 
@@ -34,11 +35,17 @@ public class AddCommand extends Command {
         }
 
         callContentEditor();
-        String description = editor.getContent();
+        try {
+            String description = editor.getContent();
+            CheatSheet cheatSheet = new CheatSheet(name, subject, description);
+            cheatSheetList.add(cheatSheet);
+            printer.printAddNewCheatSheetMessage(cheatSheet, cheatSheetList);
+        } catch (EditorException e) {
+            throw new CommandException(e.getMessage());
+        }
 
-        CheatSheet cheatSheet = new CheatSheet(name, subject, description);
-        cheatSheetList.add(cheatSheet);
-        printer.printAddNewCheatSheetMessage(cheatSheet, cheatSheetList);
+
+
     }
 
     private void callContentEditor() {
