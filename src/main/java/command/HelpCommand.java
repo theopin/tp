a8fdc1daf2c72e43fe592742bab6bee323221a3b
adhy-p <adhyp02@gmail.com>
2020-11-04 22:09:11 +0800

    Add docs

--- src/main/java/command/HelpCommand.java
+++ src/main/java/command/HelpCommand.java
@@ -2,13 +2,24 @@ package command;
 
 import ui.Printer;
 
+/**
+ * Command to display the help page.
+ */
 public class HelpCommand extends Command {
     public static final String invoker = "/help";
 
+    /**
+     * Constructor for the HelpCommand.
+     *
+     * @param printer The printer object to handle user interaction
+     */
     public HelpCommand(Printer printer) {
         super(printer);
     }
 
+    /**
+     * Prints the help page.
+     */
     @Override
     public void execute() {
         printer.printHelpSheet();
