3fd5e70172f5fc657347b5149172a66e975b45bf
abnermtj <abnermtj@live.com.sg>
2020-11-08 19:28:15 +0800

    update ui javadocs

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -2,16 +2,19 @@ package ui;
 
 import java.util.Scanner;
 
+// This class manages the input of CheatLogs.
 public class Ui {
     Scanner in = new Scanner(System.in);
-
-    public Ui() {
-    }
-
+    /**
+     * Gets the input in the terminal after the user hit <enter>
+     */
     public String getUserInput() {
         return in.nextLine();
     }
 
+    /**
+     * Closes the scanner
+     */
     public void closeScanner() {
         in.close();
     }
